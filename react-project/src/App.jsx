df587379f0d579868ed6acfcfd2247bc3d6f59d3
quickhorn <quickhorn@gmail.com>
2025-11-10 10:10:27 -0700

    Learning components.

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Eve's Kitchen</h1>
+    </header>
+  )
+}
 
 function App() {
   return (
-    <h1>hello React</h1>
-  )
+    <div>
+      <Header name="Alex"/>
+      <main>
+        <h2>We serve the most delicious food around</h2>
+      </main>
+    </div>
+  );
 }
 
 export default App
